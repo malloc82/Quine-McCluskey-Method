6dc77b6989dc94af602703d544a6239f12d96b63
Ritchie Cai <voodoochild2006@gmail.com>
2012-04-23 05:43:03 -0700

    rewrite

--- quine-mcluskey.lisp
+++ quine-mcluskey.lisp
@@ -1,4 +1,3 @@
-
 ;; Author :    Ritchie Cai
 ;; Date :      Oct 26, 2011
 ;;
@@ -17,6 +16,11 @@
     (format t "~%")
     (format t "~{~a~%~}" (reverse result))
     (format t "-------------------~%")))
+
+(defun print-table2 (table)
+  (maphash #'(lambda (key value)
+               (format t "~a : ~a ~%" key value))
+           table))
 
 (defmacro string-to-chars (str &optional pos-lst)
   (if (null pos-lst)
@@ -50,33 +54,110 @@
       counting (equal ,e elem) into total
       finally (return total)))
 
+(defun order (lst)
+  (if (or (null lst) (null (cdr lst)))
+      lst
+      (let ((pivot (first (first lst))))
+        (append (order (remove-if-not #'(lambda (x) (< (first x) pivot)) lst))
+                (remove-if-not #'(lambda (x) (= (first x) pivot)) lst)
+                (order (remove-if-not #'(lambda (x) (> (first x) pivot)) lst))))))
+
+(defun order2 (lst)
+  (if (or (null lst) (null (cdr lst)))
+      lst
+      (let ((pivot (first lst)))
+        (append (order2 (remove-if-not #'(lambda (x) (< x pivot)) lst))
+                (remove-if-not #'(lambda (x) (= x pivot)) lst)
+                (order2 (remove-if-not #'(lambda (x) (> x pivot)) lst))))))
+
+
+(defvar *xor-fn* (chars-gate :inputs (a b)
+                             :table ((0 0 0)
+                                     (0 1 1)
+                                     (1 0 1)
+                                     (1 1 0)
+                                     (- - -))))
+
+(defvar *mask-fn* (chars-gate :inputs (a b)
+                              :table ((0 0 0)
+                                      (0 1 -)
+                                      (1 0 -)
+                                      (1 1 1)
+                                      (- - -))))
+
+(defun init-table (sum-list str-format)
+  (let ((table (make-hash-table :test #'equal))
+        (count-list nil))
+    (mapc #'(lambda (x)
+              (let ((str (format nil str-format x))
+                    (chars nil))
+                (loop for c across str
+                   if (equal c #\1) count c into count
+                   do (push c chars)
+                   finally (let ((val (nreverse chars)))
+                             (push count count-list)
+                             (push val (getf (gethash count table) :current))
+                             ;; (setf (get (gethash count table) :current)
+                             ;;       (push val (get (gethash count table) :current)))
+                             ))))
+          sum-list)
+    (setf (gethash :sorted-keys table) (remove-duplicates (order2 count-list)))
+    table))
+
+(defun mark (k1 k2)
+  (let ((next-round nil)
+        (used-k1 nil)
+        (used-k2 nil))
+    (loop :for x in k1 :do
+       (loop :for y in k2 :do
+          (let ((diff (funcall *xor-fn* x y)))
+            (when (and diff
+                       (= (count-elem diff #\1) 1))
+              (push x used-k1)
+              (push y used-k2)
+              (push (funcall *mask-fn* x y) next-round)))))
+    (values
+     (remove-duplicates next-round)
+     (remove-duplicates used-k1)
+     (remove-duplicates used-k2))))
+  
+(defun qm2 (table)
+  (let ((keys (gethash :sorted-keys table)))
+    (loop
+       :for (k1 k2) :on keys :by #'cdr :until (null k2)
+       :with k1-cache = (getf (gethash (first keys) table) :current)
+       :do (when (= (- k2 k1) 1)
+             (multiple-value-bind (k1-next k1-used k2-used)
+                 (mark k1-cache (getf (gethash k2 table) :current))
+               ;; k1
+               (setf (getf (gethash k1 table) :unused)
+                     (set-difference (set-difference k1-cache k1-used)
+                                     (getf (gethash k1 table) :used)))
+               (setf (getf (gethash k1 table) :current) k1-next)
+
+               ;; k2
+               (setf (getf (gethash k2 table) :used) k2-used)
+               (setf (getf (gethash k2 table) :unused)
+                     (set-difference (getf (gethash k2 table) :current)
+                                     k2-used))
+               (setf k1-cache (getf (gethash k2 table) :current))
+               (setf (getf (gethash k2 table) :current) nil))))
+    (dolist (k keys)
+      (when (getf (gethash k table) :unused)
+        (setf (getf (gethash k table) :saved)
+              (nconc (getf (gethash k table) :unused)
+                     (getf (gethash k table) :saved)))
+        (setf (getf (gethash k table) :unused) nil)))
+    (print-table2 table)))
+
 (defun quine-mccluskey (sample-str sum-list)
   (let* ((sample-chars (string-to-chars sample-str))
          (table (make-hash-table :test #'equal))
          (bin (make-hash-table :test #'equal))
          (str-format (format nil "~c~d,'0b" #\~ (length sample-str)))
          (lst nil)
-         (final nil)
-         (xor-fn (chars-gate :inputs (a b)
-                             :table ((0 0 0)
-                                     (0 1 1)
-                                     (1 0 1)
-                                     (1 1 0)
-                                     (- - -))))
-         (mask-fn (chars-gate :inputs (a b)
-                              :table ((0 0 0)
-                                      (0 1 -)
-                                      (1 0 -)
-                                      (1 1 1)
-                                      (- - -)))))
-    (labels ((order (lst)
-               (if (or (null lst) (null (cdr lst)))
-                   lst
-                   (let ((pivot (first (first lst))))
-                     (append (order (remove-if-not #'(lambda (x) (< (first x) pivot)) lst))
-                             (remove-if-not #'(lambda (x) (= (first x) pivot)) lst)
-                             (order (remove-if-not #'(lambda (x) (> (first x) pivot)) lst))))))
-             (qm-rec (groups)
+         (final nil))
+    (labels ((qm-rec (groups)
                (when groups
                  (let ((next-round nil))
                    (mapl #'(lambda (sublst)
@@ -88,10 +169,10 @@
                                              (car (second sublst))))
                                  (mapc #'(lambda (x)
                                            (mapc #'(lambda (y)
-                                                     (let ((result (funcall xor-fn x y)))
+                                                     (let ((result (funcall *xor-fn* x y)))
                                                        (when (and result
                                                                   (= (count-elem result #\1) 1))
-                                                         (let ((new-entry (funcall mask-fn x y)))
+                                                         (let ((new-entry (funcall *mask-fn* x y)))
                                                            (when (null (gethash new-entry table))
                                                              (setf (gethash new-entry table) 0)
                                                              (push new-entry new-group))
@@ -113,7 +194,8 @@
                 (let ((str (format nil str-format x))
                       (chars nil))
                   (loop for c across str
-                     if (equal c #\1) count c into count do (push c chars)
+                     if (equal c #\1) count c into count
+                     do (push c chars)
                      finally (let ((val (nreverse chars)))
                                (setf (gethash val table) 0)
                                (push val (gethash count bin))))))
